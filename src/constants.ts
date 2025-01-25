eb5b6951d5c95a295148b77d3e1565fbf9d5b2d4
abhradeepbarman <abhradeep709@gmail.com>
2025-01-25 07:06:30 +0530

    user table added

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const UserRole = {
+    STUDENT: "STUDENT",
+    ORGANIZATION: "ORGANIZATION",
+};
--- src/db/schema/user.ts
+++ src/db/schema/user.ts
@@ -1,10 +1,14 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { UserRole } from "../../constants";
 
 const users = pgTable("users", {
-    id: serial("id").primaryKey(),
-    fullName: text("full_name"),
-    phone: varchar("phone", { length: 256 }),
-    password: varchar("password", { length: 256 }),
+    id: uuid("id").primaryKey().defaultRandom(),
+    full_name: varchar("full_name").notNull(),
+    email: varchar("email").notNull().unique(),
+    phone: varchar("phone"),
+    password: varchar("password").notNull(),
+    profilePicture: varchar("profile_picture"),
+    role: varchar("role").default(UserRole.STUDENT).notNull(),
 });
 
 export default users;
